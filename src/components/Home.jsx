84a74936f93551eda7809a252c038acbd567bb7d
Jimmie Nakadaira <jimknaka@yahoo.com>
2023-09-16 13:36:32 -0400

    created a center container for the pages

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,12 @@
 export default function Home() {
-    return <div className="portrait-container">
+    return <div className="page-container">
+        <div className="portrait-container">
         <img className="portrait" src="\src\assets\Jimmie.png" />
         <div className="personal-container">
         <div className="home-card">
         <h2 className="personal-title">Full Stack Developer</h2>
         <h1 className="personal-name">Jimmie Nakadaira</h1>
+        </div>
         </div>
         </div>
         </div>
