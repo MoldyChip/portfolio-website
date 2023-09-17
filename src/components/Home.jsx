72e2fc0730398f1153f67f6eb4e3031081f01df1
Jimmie Nakadaira <jimknaka@yahoo.com>
2023-09-16 20:27:02 -0400

    added icons for about page, fixed boxing for about/home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,10 @@ export default function Home() {
         <h2 className="personal-title">Full Stack Developer</h2>
         <h1 className="personal-name">Jimmie Nakadaira</h1>
         </div>
-        </div>
+        <h1 className='portrait-card'>
+          I am passionate about creating innovative and efficient solutions to real-world problems and thrive in collaborative team environments. Feel free to connect with me if you're interested in discussing web development projects or opportunities.
+          </h1>
+          </div>
         </div>
         </div>
 }
