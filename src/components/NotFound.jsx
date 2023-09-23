5484dab6e0d5626b9032f9c757e624a7117c2779
Jimmie Nakadaira <jimknaka@yahoo.com>
2023-09-23 13:23:40 -0400

    added a white background for notfound page

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,6 +1,6 @@
 export default function NotFound() {
     return (
-      <div>
+      <div className="not-found">
         <h2>404 - Page Not Found</h2>
         <p>The page you're looking for doesn't exist.</p>
       </div>
