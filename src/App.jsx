0f66d71412b586c8c880fbba1dadc703d330fca5
Jimmie Nakadaira <jimknaka@yahoo.com>
2023-09-23 15:05:43 -0400

    added switch

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Projects from './components/Projects'
 import About from './components/About'
 import Home from './components/Home'
 import NotFound from './components/NotFound'
-import {Route, Routes} from "react-router-dom"
+import {Switch, Route, Routes} from "react-router-dom"
 
 
 function App() {
@@ -13,10 +13,12 @@ function App() {
    <NavBar />
    <div className="container">
    <Routes>
+    <Switch>
     <Route path='/' element={<Home />} />
     <Route path='/projects' element={<Projects />} />
     <Route path='/about' element={<About />} />
     <Route path='*' element={<NotFound />} />
+    </Switch>
    </Routes>
    </div>
    </>
