20b87758dfcb7b3d5d9400dd4d8cbf8e7779ee6a
Jimmie Nakadaira <jimknaka@yahoo.com>
2023-09-23 13:58:49 -0400

    attempting to fix pathing issue

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import alias from '@rollup/plugin-alias'
+
+const projectRootDir = resolve(__dirname);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), alias],
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
         404: resolve(__dirname, "public/404.html"),
+        outDir: 'dist',
       },
     },
+  },
+  resolve: {
+    alias: {
+      "@": resolve(projectRootDir, "src"),
+    },
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 10086,
+    open: false, 
+    cors: true, 
   },
 })
