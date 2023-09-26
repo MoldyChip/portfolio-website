7fccadcd859f943d554c2077910fb8cb04581fda
Jimmie Nakadaira <jimknaka@yahoo.com>
2023-09-26 16:53:15 -0400

    created a contact me card

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,6 +12,27 @@ export default function Home() {
         <h1 className='portrait-card'>
           I am passionate about creating innovative and efficient solutions to real-world problems and thrive in collaborative team environments. Feel free to connect with me if you're interested in discussing web development projects or opportunities.
           </h1>
+          <div className='contact-card'>
+            <h2>
+              Contact Me
+            </h2>
+            <div className='email-container'>
+            <p className='email'> 
+              Email:
+            </p>
+            <p className='info'>
+              contact@jimmienakadaira.com
+            </p>
+            </div>
+            <div className='phone-container'>
+            <p className='phone'>
+              Phone Number:
+            </p>
+            <p className='info'>
+               760-455-1450
+            </p>
+            </div>
+          </div>
           </div>
         </div>
         </div>
